feeb6580f7e6222727c9e8c13f1540a88d138b04
achavez101 <amairanygchavez@gmail.com>
2023-11-16 19:01:02 -0800

    coding practice

---
+++ JS-Coding/index2.js
@@ -0,0 +1,13 @@
+// Circuit Power Exercise
+function circuitPower(voltage, current) {
+	let power;
+    return power =  voltage * current;	;
+}
+
+console.log(circuitPower(230, 10));
+console.log(circuitPower(110, 3));
+console.log(circuitPower(480, 20));
+
+circuitPower(230, 10);
+circuitPower(110, 3);
+circuitPower(480, 20);
